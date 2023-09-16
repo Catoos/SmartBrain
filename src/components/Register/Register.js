1ed7cea7fe2c2bb0d07f64639d9ad4286c964db5
Tyler Catus <catus7@yahoo.com>
2023-09-16 11:03:23 -0400

    Replaced all localhost with Render URL

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -30,7 +30,7 @@ class Register extends React.Component {
   onSubmitSignIn = () =>
   {
     try {
-    fetch('http://localhost:3000/register', {
+    fetch('https://ai-site-backend.onrender.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
